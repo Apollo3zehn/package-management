104bbe74ecdc8d6ddca053bc48662bf5c936a9c1
Vincent Wilms <git@m1.apollo3zehn.net>
2025-01-17 16:13:42 +0100

    Add missing AddExtensionHive method

--- src/package-management/dotnet/Core/PackageManagementServiceCollectionExtensions.cs
+++ src/package-management/dotnet/Core/PackageManagementServiceCollectionExtensions.cs
@@ -22,4 +22,16 @@ public static class MvcServiceCollectionExtensions
             .AddSingleton<IPackageManagementPathsOptions>(
                 serviceProvider => serviceProvider.GetRequiredService<IOptions<PackageManagementPathsOptions>>().Value);
     }
+
+    /// <summary>
+    /// Adds a generic extension hive.
+    /// </summary>
+    /// <typeparam name="T">The base type of the extensions to host.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddExtensionHive<T>(this IServiceCollection services)
+        where T : class
+    {
+        return services.AddSingleton<IExtensionHive<T>, ExtensionHive<T>>();
+    }
 }
